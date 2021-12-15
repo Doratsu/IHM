4be22d18020e906c4660e59c8ce2fa255d84fda0
Doratsu <stanislas.merlevede@gmail.com>
2021-12-15 18:09:08 +0100

    update

--- my-app/src/Pages/NavBar/Cours.js
+++ my-app/src/Pages/NavBar/Cours.js
@@ -5,6 +5,8 @@ import "../../Styles/Cours.css";
 import { datasCours } from "../../Datas/datasCours";
 import CardsCours from "../../Components/CardsCours";
 import MenuListCours from "../../Components/MenuListCours";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 export default class Cours extends Component {
   constructor(props, context) {
@@ -22,20 +24,30 @@ export default class Cours extends Component {
     }, []);
 
     return (
-      <div>
-        <MenuListCours />
-        {categories.map((category) => {
-          const datasCoursFiltered = datasCours.filter(
-            (prod) => prod.category === category
-          );
-          return (
-            <CardsCours
-              datasCours={datasCoursFiltered}
-              onClick={this.handleClick.bind(this)}
-            />
-          );
-        })}
-      </div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+          <Grid container item spacing={2}>
+            <React.Fragment>
+              <Grid item xs={2}>
+                <MenuListCours />
+              </Grid>
+              <Grid item xs={9}>
+                {categories.map((category) => {
+                  const datasCoursFiltered = datasCours.filter(
+                    (prod) => prod.category === category
+                  );
+                  return (
+                    <CardsCours
+                      datasCours={datasCoursFiltered}
+                      onClick={this.handleClick.bind(this)}
+                    />
+                  );
+                })}
+              </Grid>
+            </React.Fragment>
+          </Grid>
+        </Grid>
+      </Box>
     );
   }
 }
--- my-app/src/Pages/NavBar/Decouvrir.js
+++ my-app/src/Pages/NavBar/Decouvrir.js
@@ -5,6 +5,8 @@ import "../../Styles/Decouvrir.css";
 import { datasDecouv } from "../../Datas/datasDecouv.js";
 import CardsDecouvrir from "../../Components/CardsDecouvrir";
 import MenuListDecouvrir from "../../Components/MenuListDecouvrir";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 export default class Decouvrir extends Component {
   constructor(props, context) {
@@ -22,16 +24,25 @@ export default class Decouvrir extends Component {
     }, []);
 
     return (
-      <div>
-        <MenuListDecouvrir />
-
-        {categories.map((category) => {
-          const datasDecouvFiltered = datasDecouv.filter(
-            (prod) => prod.category === category
-          );
-          return <CardsDecouvrir datasDecouv={datasDecouvFiltered} />;
-        })}
-      </div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+          <Grid container item spacing={2}>
+            <React.Fragment>
+              <Grid item xs={2}>
+                <MenuListDecouvrir />
+              </Grid>
+              <Grid item xs={9}>
+                {categories.map((category) => {
+                  const datasDecouvFiltered = datasDecouv.filter(
+                    (prod) => prod.category === category
+                  );
+                  return <CardsDecouvrir datasDecouv={datasDecouvFiltered} />;
+                })}
+              </Grid>
+            </React.Fragment>
+          </Grid>
+        </Grid>
+      </Box>
     );
   }
 }
